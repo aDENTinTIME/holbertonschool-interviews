ba9bddc919e6ac1dd846b556325625dbfcdbe5bd
adentintime <arixbizz@gmail.com>
2018-10-09 06:57:06 +0000

    Create base files for slide_line

---
+++ 0x01-warm_up/0-slide_line.c
@@ -0,0 +1,15 @@
+#include "slide_line.h"
+
+
+/**
+ * slide_line - Acts like 2048.
+ *
+ * @line: Array.
+ * @size: Length.
+ * @direction: Direction to slide.
+ * Return: 1 upon success, 0 upon failure.
+ */
+int main(void)
+{
+	return (1);
+}
---
+++ 0x01-warm_up/slide_line.h
@@ -0,0 +1,8 @@
+#ifndef SLIDE_LINE_H
+#define SLIDE_LINE_H
+
+#include <stdio.h>
+
+int slide_line(int *line, size_t size, int direction);
+
+#endif /* SLIDE_LINE_H */
