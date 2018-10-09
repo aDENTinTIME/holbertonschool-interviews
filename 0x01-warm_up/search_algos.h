01955176b34c72db8ae2f42a89f1f9dbaf7357bc
adentintime <arixbizz@gmail.com>
2018-10-09 06:25:35 +0000

    Update header with prototype

--- 0x01-warm_up/search_algos.h
+++ 0x01-warm_up/search_algos.h
@@ -22,5 +22,6 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 
 int binary_search(int *array, size_t size, int value);
+int binary_tree_is_full(const binary_tree_t *tree);
 
 #endif /* SEARCH_ALGOS_H */
